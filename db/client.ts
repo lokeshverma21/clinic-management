69a8e91704fb068c4cd4378db03c6f1b0fe09bd2
lokeshverma21 <lokesh21980@gmail.com>
2026-07-05 17:47:15 +0530

    refactor: updated db/client.ts to support transactions with pool

--- db/client.ts
+++ db/client.ts
@@ -4,13 +4,15 @@ dotenv.config({
   path: ".env.local",
 });
 
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-export const db = drizzle(sql,{
-    schema,
+export const db = drizzle(pool, {
+  schema,
 });
