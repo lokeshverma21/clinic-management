da4ff3d7b325999a418ea05d322e7223782e8381
lokeshverma21 <lokesh21980@gmail.com>
2026-07-07 18:55:41 +0530

    feat: added shadcn UI and sidebar in dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,37 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
+
 export default function DashboardLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return <section>{children}</section>
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+
+      <SidebarInset>
+        <header className="flex h-16 items-center gap-2 border-b px-4">
+          <SidebarTrigger />
+
+          <Separator
+            orientation="vertical"
+            className="h-4"
+          />
+
+          {/* Later you can add breadcrumbs, search, profile, etc. */}
+          <h1 className="font-medium">ClinicOS</h1>
+        </header>
+
+        <main className="flex-1 p-6">
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
